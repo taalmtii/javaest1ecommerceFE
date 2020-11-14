01c7d0adac415f0962cb8cce5df56fd300472d49
Guilherme Quirino <guilhermezq@gmail.com>
2020-11-14 10:06:20 +0200

    route for product list

--- javaest1ecommerceFE/src/app/app-routing.module.ts
+++ javaest1ecommerceFE/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsListComponent } from './products-list/products-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+    {path: "", component: ProductsListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
